b051a5b9caaba92f1210da8d5e29aed942737301
Daniel <daniel.r.sousa1911@gmail.com>
2022-05-04 10:18:57 -0300

    .

--- client/src/components/Contact/ContactInfo.jsx
+++ client/src/components/Contact/ContactInfo.jsx
@@ -11,9 +11,12 @@ const ContactInfo = () => {
                     </div>
 
                     <div className="contact-info-text">
-                        <h3>(99) 999189-4579</h3>
-                        <h3>daniel.r.sousa1911@gmail.com</h3>
-                        <h3>https://github.com/rdanielrs</h3>
+                        <p className='contact-text'>(99) 999189-4579</p>
+                        <p className='contact-text'>daniel.r.sousa1911@gmail.com</p>
+                        <a href="https://github.com/rdanielrs" className="github-link">https://github.com/rdanielrs</a>
+                        <br />
+                        <br />
+                        <a href="https://www.linkedin.com/in/daniel-rodrigues-78b732224/" className='github-link'>LinkedIn</a>
                     </div>
                 </div>
             </div>
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+//import HeaderGlobal from '../../components/Global/HeaderGlobal';
+import HeaderGlobal from '../components/Global/HeaderGlobal'
+import ContactInfo from '../components/Contact/ContactInfo';
+import '../styles/Contact.css'
 
 const Contact = () => {
     return(
         <>
-            <div className="container-global-header"></div>
+            <div className="container-global-header">
+                <HeaderGlobal />
+            </div>
+
+            <div className="contact-info-component">
+                <ContactInfo />
+            </div>
+
         
         </>
     )
