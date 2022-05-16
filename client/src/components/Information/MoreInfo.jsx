7cdac04128edec68ca1f0fdea6fd4ed77a973e03
Daniel <daniel.r.sousa1911@gmail.com>
2022-05-16 10:36:12 -0300

    pagina sobre atualizada

--- client/src/components/Information/MoreInfo.jsx
+++ client/src/components/Information/MoreInfo.jsx
@@ -17,8 +17,9 @@ const MoreInfo = () => {
                             </div>
 
                             <div className="info-sub-description">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, maiores eveniet non magnam, dolores vero magni vitae numquam deserunt corporis aliquid ex dicta eaque inventore quos nulla, et fuga illo.</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam reiciendis architecto possimus modi totam consequuntur voluptatibus eveniet quae minus explicabo sequi tempora, inventore voluptate laboriosam! Expedita, magnam officiis! Minima, consequatur.</p>
+                                <p>Para utilizar as funcionalidades do site, crie uma conta, com um nome de usuário único, e adicione "pastas" que contém arquivos.</p>
+                                <p>Arquivos podem conter linhas de código inseridas pelo usuário, que podem ser lidas e copiadas a qualquer momento.</p>
+                                <p>Pastas de arquivos também podem ser filtradas. Atualmente, o site conta com 3 atributos de filtro: a-z, data de criação e tamanho.</p>
                             </div>
                         </div>
 
@@ -28,28 +29,11 @@ const MoreInfo = () => {
                             </div>
 
                             <div className="info-sub-description">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, maiores eveniet non magnam, dolores vero magni vitae numquam deserunt corporis aliquid ex dicta eaque inventore quos nulla, et fuga illo.</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam reiciendis architecto possimus modi totam consequuntur voluptatibus eveniet quae minus explicabo sequi tempora, inventore voluptate laboriosam! Expedita, magnam officiis! Minima, consequatur.</p>
+                                <p>O objetivo deste site é criar uma plataforma que permite o armazenamento organizado e padronizado de blocos de código, facilitando a reutilização dos mesmos para futura ocasiões.</p>
+                                <p>Exemplo: durante o desenvolvimento de uma aplicação web, um programador precisa criar um requisito que efetua o login dos usuários. Já tendo esse código salvo em uma pasta de arquivos de sua escolha, basta que ele seja copiado e adaptado para a aplicação atual, sem que o requisito seja criado "do zero".</p>
                             </div>
                         </div>
-
-                        <div className="sub-section">
-                            <div className="info-sub-title">
-                                <h2>Metodologia</h2>
-                            </div>
-
-                            <div className="info-sub-description">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, maiores eveniet non magnam, dolores vero magni vitae numquam deserunt corporis aliquid ex dicta eaque inventore quos nulla, et fuga illo.</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam reiciendis architecto possimus modi totam consequuntur voluptatibus eveniet quae minus explicabo sequi tempora, inventore voluptate laboriosam! Expedita, magnam officiis! Minima, consequatur.</p>
-                            </div>
-                        </div>
-
-                        
-
                     </div>
-                    
-
-
                 </div>
             </div>
         </>
